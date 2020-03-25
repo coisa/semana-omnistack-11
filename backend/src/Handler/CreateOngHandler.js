5002eddb2488e46ae9cc5bf6eae764a92340ca0a
Felipe Sayão Lobato Abreu <github@felipeabreu.com.br>
2020-03-25 14:28:47 -0300

    Trailing whitespace

--- backend/src/Handler/CreateOngHandler.js
+++ backend/src/Handler/CreateOngHandler.js
@@ -2,7 +2,7 @@ const OngService = require('../Service/OngService');
 
 module.exports = async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body;
-    
+
     const id = await OngService.create(name, email, whatsapp, city, uf);
 
     response.status(201);
